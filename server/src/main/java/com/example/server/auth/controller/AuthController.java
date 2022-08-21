cfb2fe5076dcf28b80b87dafb97500770386955e
DESKTOP-VM7P796\admin <rustam.gazizov@bpium.ru>
2022-08-21 16:28:58 +0300

    api auth endpoints

--- server/src/main/java/com/example/server/auth/controller/AuthController.java
+++ server/src/main/java/com/example/server/auth/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.example.server.auth.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +11,18 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/api/auth")
 public class AuthController {
   
+  @PostMapping("/login")
+  public void login() {
+
+  }
+
+  @PostMapping("/logout")
+  public void logout() {
+
+  }
+
+  @PostMapping("token/refresh")
+  public void refresh() {
+    
+  }
 }
